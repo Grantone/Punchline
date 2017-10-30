c6652d1fdf4d5264cc11ec5cf909e6e6f8316322
Granton Mchana <gmchana254@gmail.com>
2017-10-30 12:31:36 +0300

    created a configuration file

--- app/views.py
+++ app/views.py
@@ -2,8 +2,8 @@ from flask import render_template
 from app import app
 
 # views
-@app.route('/')
+@app.route('/category/<int:category_id>')
 def index():
 
     message = 'One Minute Pitch'
-    return render_template('index.html',message = message)
+    return render_template('category.html',id = category_id)
