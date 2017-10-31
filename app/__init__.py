d5ca13d19bb46a0ed33ffbe879df5d1eba200c47
Granton Mchana <gmchana254@gmail.com>
2017-10-31 10:04:30 +0300

    additions

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,22 @@
 from flask import Flask
 from .config import DevConfig
+from flask_sqlallchemy import SQLAlchemy
 
 
 # Initializing app
+def create_app(config_name):
 app = Flask(__name__)
 
 
 # Setting up configurations
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+bootstrap.init_app(app)
+db.init_app(app)
 
 
 from app import views
--- app/models.py
+++ app/models.py
@@ -1,11 +1,14 @@
-class Category:
+class Category:(db.Model)
     __tablename__ = 'categories'
 
     id = db.column(db.Integer,primary_key = True)
 
 
-class User():
+class User(db.Model):
     __tablename__ = 'users'
 
     id = db.column(db.Integer,primary_key = True)
     name = db.Column(db.string(255))
+
+def __repr__(self):
+    return f'User {self.name}'
