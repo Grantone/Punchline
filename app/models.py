73ed1e8be09fad83c22b490eaa758a26589a4109
Granton Mchana <gmchana254@gmail.com>
2017-10-31 09:17:40 +0300

    created classes in model

--- app/models.py
+++ app/models.py
@@ -1,1 +1,13 @@
-from . import db
+class Category:
+
+
+    def __init__(self,id,title,date):
+        self.id = id
+        self.title = title
+        self.time = time
+
+
+class User():
+    __tablename__ = 'users'
+
+    id = db.column(db.Integer,primary_key = True)
