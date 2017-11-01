98930668862a60e87c0edf65ef159e5d971c1489
Granton Mchana <gmchana254@gmail.com>
2017-11-01 16:19:41 +0300

    created auth file inside templates folder

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,3 +1,9 @@
+from flask import render_template
 from . import auth
 from ..models import User
 from .. import db
+
+
+@auth.route('/login')
+def login():
+    return render_template('auth/login.html')
