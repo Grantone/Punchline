49e7ae8a3703030e4765feafbaefec56902d91fa
Granton Mchana <gmchana254@gmail.com>
2017-11-01 16:14:59 +0300

    created a password setter in models file

--- app/models.py
+++ app/models.py
@@ -24,6 +24,18 @@ class User(db.Model):
     db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     comments = db.relationship('Comment', backref='user', lazy="dynamic")
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+        def verify_password(self, password):
+            return check_password_hash(self.pass_secure, password)
 
     def __repr__(self):
         return f'User {self.name}'
