6c9edc30b200ad4b4643f74e5a814edd2a32e90a
Granton Mchana <gmchana254@gmail.com>
2017-11-01 17:00:59 +0300

    created a registration form

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,8 @@ from flask import render_template, redirect, url_for
 from . import auth
 from ..models import User
 from .. import db
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
+from flask_login import login_user
 
 
 @auth.route('/login')
