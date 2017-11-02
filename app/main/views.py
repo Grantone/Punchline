6a788a11d8c699db056b3ef4c33f421398aa2b65
Granton Mchana <gmchana254@gmail.com>
2017-11-02 08:45:02 +0300

    dletions

--- app/main/views.py
+++ app/main/views.py
@@ -10,20 +10,8 @@ from flask_login import login_required
 @main.route('/')
 def index():
 
-    #
-    # #categories
-    # pickup_lines = get_categories('pickupLine')
-    # interview_pitch = get_categories('interviewPitch')
-    # product_pitch = get_categories('productPitch')
-    # promotion_pitch = get_categories('promotionPitch')
-    #
-    # ,title = title, punchLine = punchLine_categories, interviewPitch = interviewPitch_categories, productPitch = productPitch_categories, promotionPitch = promotionPitch_categories
-
     title = 'Home - Welcome to One Minute Pitch web'
     return render_template('index.html', title=title)
-
-    # message = 'One Minute Pitch'
-    # return render_template('category.html',id = category_id)
 
 
 @main.route('/category/comment/new/<int:id>', methods=['GET', 'POST'])
