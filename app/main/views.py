772cacfd711db3e0e897f6430c792e1d34706141
Granton Mchana <gmchana254@gmail.com>
2017-11-06 18:39:42 +0300

    debbuging

--- app/main/views.py
+++ app/main/views.py
@@ -23,7 +23,7 @@ def new_comment(id):
     if form.validate_on_submit():
         title = form.title.data
         comment = form.comment.data
-        new_comment = Comment(category.id, title, comment)
+        new_comment = Comments(category.id, title, comment)
         new_comment.save_comment()
         return redirect(url_for('category', id=category.id))
 
@@ -39,3 +39,25 @@ def category(id):
     comments = Comment.get_comments(category.id)
 
     return render_template('category.html', title=title, category=category, comments=comments)
+
+
+@main.route('/pitch/new/<int:id>', methods=["GET", "POST"])
+@login_required
+def new_comment(id):
+    pitch = Pitch.query.filter_by(id=id).first()
+    if pitch is None:
+        abort(404)
+
+    form = CommentForm()
+
+    if form.validate_on_submit():
+        body = form.body.data
+        new_comment = Comments(
+            body=body, pitch_id=pitch.id, user_id=current_user.id)
+
+        new_comment.save_comment()
+
+        return redirect(url_for('.single_pitch', id=pitch.id))
+
+    title = "New Comment"
+    return render_template('new_comment.html', comment_form=form, title=title)
